64b976c290ca904ceb842fe49c4af5aa6d90e0da
nmobassir <mr.naib@gmail.com>
2015-04-13 16:36:34 -0400

    sql->json php conversion script

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+$link = mysql_pconnect("localhost", "root", "secret") or die("Could not connect");
+mysql_select_db("mhealth") or die("Could not select database");
+ 
+$arr = array();
+ 
+$rs = mysql_query("SELECT * FROM xmlsitemap");
+ 
+while($obj = mysql_fetch_object($rs)) {
+$arr[] = $obj;
+}
+echo '{"members":'.json_encode($arr).'}';
+
+?>

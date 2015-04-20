6fbc80f28d360733fb02e5bebe2ff809f2ce196f
nmobassir <mr.naib@gmail.com>
2015-04-20 14:47:03 -0400

    json files

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ mysql_select_db("mhealth") or die("Could not select database");
  
 $arr = array();
  
-$rs = mysql_query("SELECT * FROM xmlsitemap");
+$rs = mysql_query("SELECT * FROM users");
  
 while($obj = mysql_fetch_object($rs)) {
 $arr[] = $obj;
